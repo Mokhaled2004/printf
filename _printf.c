7c1a8eb140d72dc2b8b7e80fa666e33520acde04
mazenwaelx <mazenwael575@gmail.com>
2023-08-22 06:38:43 -0700

    lol

--- _printf.c
+++ _printf.c
@@ -37,8 +37,8 @@ p = get_precision(p, &params, ap);
 if (get_modifier(p, &params))
 p++;
 if (!get_specifier(p))
-sum += print_from_to(start, p, params.l_modifier
-|| params.h_modifier ? p - 1 : 0);
+sum += print_from_to(start, p
+, params.l_modifier || params.h_modifier ? p - 1 : 0);
 else
 sum += get_print_func(p, ap, &params);
 }
