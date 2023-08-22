896229db29dc648ddcd1f406ad6fd71ead455343
Mokhaled2004 <mokhaled2004@gmail.com>
2023-08-22 06:04:35 -0700

    Aloooo

--- specifier.c
+++ specifier.c
@@ -8,13 +8,23 @@
 */
 int (*get_specifier(char *s))(va_list ap, params_t *params)
 {
-specifier_t specifiers[] =
-{
-{"c", print_char},{"d", print_int},{"i", print_int},{"s", print_string},
-{"%", print_rev},{"b", print_binary},{"o", print_octal},{"u", print_unsigned},
-{"x", print_hex},{"X", print_HEX},{"p", print_address},{"S", print_S},{"r", print_rev},
-{"R", print_tot13},{NULL, NULL}};
-int i =0 ;
+specifier_t specifiers[] = {
+{"c", print_char},
+{"d", print_int},
+{"i", print_int},
+{"s", print_string},
+{"%", print_rev},
+{"b", print_binary},
+{"o", print_octal},
+{"u", print_unsigned},
+{"x", print_hex},
+{"X", print_HEX},
+{"p", print_address},
+{"S", print_S},
+{"r", print_rev},
+{"R", print_tot13},
+{NULL, NULL}};
+int i = 0;
 while (specifiers[i].specifier)
 {
 if (*s == specifiers[i].specifier[0])
@@ -34,7 +44,7 @@ return (NULL);
 *
 * Return: the number of bytes printd
 */
-int get_print_func(char *s ,va_list ap , params_t *params)
+int get_print_func(char *s, va_list ap, params_t *params)
 {
 int (*f)(va_list, params_t *) = get_specifier(s);
 if (f)
@@ -49,7 +59,7 @@ return (0);
 *
 * Return: if flag was valid
 */
-int get_flag(char *s,params_t *params)
+int get_flag(char *s, params_t *params)
 {
 int i = 0;
 switch (*s)
@@ -81,17 +91,17 @@ return (i);
 */
 int get_modifier(char *s, params_t *params)
 {
-int i =0;
+int i = 0;
 switch (*s)
 {
 case 'h':
-i=params->h_modifier = 1;
+i = params->h_modifier = 1;
 break;
 case 'l':
-i=params->l_modifier = 1;
+i = params->l_modifier = 1;
 break;
 }
-return(i);
+return (i);
 }
 
 /**
@@ -101,9 +111,9 @@ return(i);
 * @ap: gg
 * Return: new pointer
 */
-char *get_width(char *s,params_t *params, va_list ap)
+char *get_width(char *s, params_t *params, va_list ap)
 {
-int d = 0 ;
+int d = 0;
 if (*s == '*')
 {
 d = va_arg(ap, int);
@@ -111,7 +121,7 @@ s++;
 }
 else
 {
-while(_isdigit(*s))
+while (_isdigit(*s))
 d = d * 10 + (*s++ - '0');
 }
 params->width = d;
